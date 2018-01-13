a79034cecc936d2fbb4e4d86a92541b868b9e838
Raviraj <pandit.raviraj21@gmail.com>
2018-01-13 16:00:29 +0530

    conflict resolved

--- first try/try.py
+++ first try/try.py
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 print 'good night'
-<<<<<<< HEAD
+
 
 print 'it is evening'
-=======
+
 print 'good afternoon'
->>>>>>> 5ea7a6f2395b55fdb2f3d4bf0c3dad6705579760
-=======
-print 'Good night'
-print 'Good afternoon'
->>>>>>> 572f4b455924fb50396330d4ece077851477d766
+
+
+
+
